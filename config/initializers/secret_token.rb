9bd3846b28a2302184008d972fa28c89f3b332e6
Ivan Kovalisko <ikovalisko@gmail.com>
2012-08-25 18:47:53 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paciorky::Application.config.secret_token = '9a526bf74837a744b00dc57ba4bf92b4bbd150dd39a763a069c031414e255607a7c6e8c26d12a0bf00cda15a5259b5bf6ddb964f099bf0e385e2cb196acecad2'
