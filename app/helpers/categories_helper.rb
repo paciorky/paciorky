fd687a61b82b8d3b14f600c7b7bb649fc55ff20d
Ivan Kovalisko <ikovalisko@gmail.com>
2012-09-09 02:21:52 +0300

    navigation bar and footer

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  def active_category_class(category_id = 0)
+      'active' if active_category(category_id)
+    end
+
+    def active_category(category_id = 0)
+      (controller_name == "categories") && (category_id == params[:id].to_i)
+    end
 end
