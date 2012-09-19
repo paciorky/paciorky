908133ef373dcb3599928aea619455b7d56644ff
Ivan Kovalisko <ikovalisko@gmail.com>
2012-09-20 00:38:30 +0300

    new design for thumbs

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,5 +2,5 @@ class Asset < ActiveRecord::Base
   attr_accessible :asset
 
   belongs_to :item
-  has_attached_file :asset, :styles => { :large => "600x", :medium => "158x158#", :thumb => "73x" }
+  has_attached_file :asset, :styles => { :large => "615x", :medium => "150x150#", :thumb => "73x" }
 end
