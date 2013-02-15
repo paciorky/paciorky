68e0dd20ae48979055e9c473b11aea85c4cf2b93
Ivan Kovalisko <ikovalisko@gmail.com>
2013-02-15 22:15:12 +0200

    Started writing specs for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -7,5 +9,5 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe Item do
+  context 'relations' do
+    it { should belong_to :category }
+    it { should have_many :assets }
+  end
+
   context 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
@@ -10,5 +15,11 @@ describe Item do
     it { should ensure_length_of(:name).is_at_least(3).is_at_most(100) }
     it { should ensure_length_of(:description).is_at_least(50) }
     it { should validate_numericality_of(:price) }
+  end
+
+  context 'instance methods' do
+    before(:all) do
+      @item = FactoryGirl.build :item
+    end
   end
 end
